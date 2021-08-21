0e20264ddba7217cfc91f5d1db7e03f2158c0d0d
kumarvna <36370570+kumarvna@users.noreply.github.com>
2021-08-21 22:48:45 +0530

    adding outputs

--- examples/complete/output.tf
+++ examples/complete/output.tf
@@ -1,19 +1,19 @@
 output "nat_gateway_id" {
   description = "The ID of the NAT Gateway"
-  value       = [for k in azurerm_nat_gateway.main : k.id]
+  value       = module.nat-gateway.nat_gateway_id
 }
 
 output "nat_gateway_resource_guid" {
   description = "The resource GUID property of the NAT Gateway"
-  value       = [for k in azurerm_nat_gateway.main : k.resource_guid]
+  value       = module.nat-gateway.nat_gateway_resource_guid
 }
 
 output "azurerm_nat_gateway_public_ip_association_id" {
   description = "The (Terraform specific) ID of the Association between the Nat Gateway and the Public IP."
-  value       = [for k in azurerm_nat_gateway_public_ip_association.main : k.id]
+  value       = module.nat-gateway.azurerm_nat_gateway_public_ip_association_id
 }
 
 output "azurerm_nat_gateway_public_ip_prefix_association_id" {
   description = " The (Terraform specific) ID of the Association between the Nat Gateway and the Public IP Prefix."
-  value       = [for k in azurerm_nat_gateway_public_ip_prefix_association.main : k.id]
+  value       = module.nat-gateway.azurerm_nat_gateway_public_ip_prefix_association_id
 }
